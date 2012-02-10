10ce96ee19817952ea7e49467c5c3fd476096dd8
Nick Parfene <nick@tmro.net>
2012-02-11 12:10:25 +1300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Askaway::Application.config.secret_token = '82d1ffb9483795a2c734ad94264bfad3fc461d65f4e4b6671d8afa7e64efc13d57e434fa96ea2bb086daf9b8c0ad36fa45b3949875625089da1a5d02b3b0f0f2'
